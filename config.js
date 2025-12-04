789b2804a5712e0accf97067dcac0d8480c8bdfd
alcivarcastillor88-stack <alcivarcastillor88@gmail.com>
2025-12-03 21:07:58 -0500

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const API = {
   API_URL: "https://script.google.com/macros/s/AKfycbxeGWzL4CxBxD-CXYyculNu_vQ71ddCB8Q163NKGmlEWVC-uxMSOJmQIwLcMekeH34M/exec",
-  SECRET_KEY: "AKfycbxeGWzL4CxBxD-CXYyculNu_vQ71ddCB8Q163NKGmlEWVC-uxMSOJmQIwLcMekeH34M"
+  SECRET_KEY: "TU_LLAVE_SECRETA"
 };
+
 
