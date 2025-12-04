c7e2c2fea30b43b3916f74d599713e1fd0c7fbb8
alcivarcastillor88-stack <alcivarcastillor88@gmail.com>
2025-12-03 20:10:45 -0500

    preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,9 +1,13 @@
 const preguntas = {
   matematicas: [
-    { p: "¿Cuánto es 2 + 2?", o: ["1", "2", "4", "5"], r: 2 },
+    { p: "¿Cuál es el valor de π aproximado a dos decimales?", o: ["a) 3.12", "b) 3.14", "c) 3.16", "d) 3.18"], r: 2 },
+  { p: "¿Cuál es la raíz cuadrada de 144?", o: ["a) 10", "b) 11", "c) 12", "d) 13"], r: 3 },
+  { p: "¿Cuántos grados tiene un ángulo recto?", o: ["a) 45°", "b) 90°", "c) 120°", "d) 180°"], r: b },
+  { p: "¿Cuánto es 7 × 8?", o: ["a) 48", "b) 54", "c) 56", "d) 64"], r: 3 }
     { p: "¿Cuánto es 10 × 2?", o: ["20","10","5","12"], r: 0 }
   ],
   historia: [
     { p: "¿En qué año descubrió Colón América?", o: ["1492","1776","1945","1810"], r: 0 }
   ]
 };
+
