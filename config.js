2b1f8b478bb39bd73110e640a0c93f5db8c63ae5
alcivarcastillor88-stack <alcivarcastillor88@gmail.com>
2025-12-03 21:02:04 -0500

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const API = {
-  API_URL: "AQUI_TU_URL_DE_APPS_SCRIPT",
-  SECRET_KEY: "AQUI_TU_LLAVE_SECRETA"
+  API_URL: "https://script.google.com/macros/s/AKfycbxeGWzL4CxBxD-CXYyculNu_vQ71ddCB8Q163NKGmlEWVC-uxMSOJmQIwLcMekeH34M/exec",
+  SECRET_KEY: "AKfycbxeGWzL4CxBxD-CXYyculNu_vQ71ddCB8Q163NKGmlEWVC-uxMSOJmQIwLcMekeH34M"
 };
+
