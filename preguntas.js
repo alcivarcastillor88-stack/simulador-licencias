bad80fe2608a24b522c7df90c1fc5943ef0bdbd6
alcivarcastillor88-stack <alcivarcastillor88@gmail.com>
2025-12-03 19:54:30 -0500

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,9 @@
+const preguntas = {
+  matematicas: [
+    { p: "¿Cuánto es 2 + 2?", o: ["1", "2", "4", "5"], r: 2 },
+    { p: "¿Cuánto es 10 × 2?", o: ["20","10","5","12"], r: 0 }
+  ],
+  historia: [
+    { p: "¿En qué año descubrió Colón América?", o: ["1492","1776","1945","1810"], r: 0 }
+  ]
+};
